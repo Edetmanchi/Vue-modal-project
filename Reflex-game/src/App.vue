4f2254376305c4c67e5973156b8c5b67e74f1876
Edet Effiong <greatstory247@gmail.com>
2025-01-27 14:59:45 +0100

    All app funtionality created

---
+++ Reflex-game/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Reflex Timer Game</h1>
+    <button @click="start" :disabled='isPlaying' class="play">Play</button>
+      <Block v-if="isPlaying" :delay="delay" @end='endGame'/>
+      <Result v-if="showResult" :score="score"/>
+  </div>
+</template>
+
+<script >
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+  export default{
+    components:{
+      Block,
+      Result,
+    },
+    props: [],
+    data(){
+     return {
+       isPlaying: false,
+       delay: null,
+       score: null,
+       showResult: false
+    }
+    },
+    methods:{
+      start (){
+        this.isPlaying = true,
+        this.delay = 2000 + Math.random() + 4000
+        this.showResult=false
+      },
+      endGame(reactionTime){
+        this.score = reactionTime,
+        this.isPlaying = false
+        this.showResult= true
+      }
+    }
+  }
+</script>
+
+
+
+
+<style scoped>
+ h1 {
+  font-size: 50px;
+  text-align: center;
+}
+.play{
+  padding: 5px 20px;
+  border: 2px gray solid;
+}
+.disabled{
+  border: 1px black;
+}
+@media (min-width: 1024px) {
+  
+}
+</style>
---
+++ Reflex-game/src/components/Result.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="container">
+    <span class="results">Your Reaction time is {{score}} ms</span>
+    <p >{{rank}}</p>
+</div>
+  
+</template>
+
+<script>
+    export default {
+        props: ['score'],
+        data(){
+            return {
+             rank: null
+
+            }
+
+        },
+        mounted(){
+            if(this.mounted < 250){
+                this.rank = "Phew..... you've got a  ninja finger"
+            }
+            else if(this.score < 450){
+                this.rank = "Rapid Reflexes"
+            }
+            else{
+                this.rank = 'Snail pace'
+            }
+        }
+
+}
+</script>
+
+<style scoped>
+.results {
+    background-color: grey;
+    display: inline-blockx;
+    width: 800px;
+    padding: 10 30px;
+}
+.container {
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
